b17b04ff14ae66ade5278400488aa9ac1fa5e19e
Joan Fisbein <joan@fon.com>
2016-02-08 18:49:54 +0100

    Renamed memcached methods

--- lib-memcache.sh
+++ lib-memcache.sh
@@ -9,9 +9,9 @@
 #    source membash.sh
 #    MCSERVER="localhost"
 #    MCPORT=11211
-#    foobar=$(mc_get foobar)
+#    foobar=$(mc-get foobar)
 #    [ -z "$foobar" ] && foobar="default value"
-#    mc_set foobar 0 "$foobar"
+#    mc-set foobar 0 "$foobar"
 
 # original author: wumin, https://gist.github.com/ri0day/1538831
 # updated by goodevilgenius to support debian-based systems, support more
@@ -28,7 +28,7 @@ function lib-memcache-init() {
 	MCPORT=${2:-11211}
 }
 
-mc_usage() {
+mc-usage() {
 	format_usage="membash: a memcache library for BASH \n\
 https://gist.github.com/goodevilgenius/11375877\n\n\
 Usage:\n
@@ -43,43 +43,43 @@ Commands: \n \
 	\t get key \n \
 	\t delete key [time] \n \
 	\t stats \n \
-	\t list_all_keys"
+	\t list-all-keys"
 	echo -e $format_usage
 }
 
-mc_help() { mc_usage;}
+mc-help() { mc-usage;}
 
-mc_sendmsg() { echo -e "$*\r" | nc $MCSERVER $MCPORT | tr -d '\r';}
+mc-sendmsg() { echo -e "$*\r" | nc $MCSERVER $MCPORT | tr -d '\r';}
 
-mc_stats() { mc_sendmsg "stats";}
+mc-stats() { mc-sendmsg "stats";}
 
-mc_get_last_items_id() {
-	local LastID=$(mc_sendmsg "stats items"|tail -n 2|head -n 1|awk -F':' '{print $2}')
+mc-get-last-items-id() {
+	local LastID=$(mc-sendmsg "stats items"|tail -n 2|head -n 1|awk -F':' '{print $2}')
 	echo $LastID
 }
 
-mc_list_all_keys() {
-	:>/dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt
-	local max_item_num=$(mc_get_last_items_id)
+mc-list-all-keys() {
+	:>/dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt
+	local max_item_num=$(mc-get-last-items-id)
 	for i in `seq 1 $max_item_num`; do
-		mc_sendmsg "stats cachedump $i 0" | awk '{print $2}'
-	done >>/dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt
-	sed -i '/^$/d' /dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt
-	cat /dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt
+		mc-sendmsg "stats cachedump $i 0" | awk '{print $2}'
+	done >>/dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt
+	sed -i '/^$/d' /dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt
+	cat /dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt
 }
 
-mc_get() { mc_sendmsg "get $1" | awk "/^VALUE $1/{a=1;next}/^END/{a=0}a" ;}
+mc-get() { mc-sendmsg "get $1" | awk "/^VALUE $1/{a=1;next}/^END/{a=0}a" ;}
 
-mc_touch() {
+mc-touch() {
 	local key="$1"
 	shift
 	local exptime
 	let exptime="$1"
 	shift
-	mc_sendmsg "touch $key $exptime"
+	mc-sendmsg "touch $key $exptime"
 }
 
-mc_doset() {
+mc-doset() {
 	local command="$1"
 	shift
 	local key="$1"
@@ -90,29 +90,29 @@ mc_doset() {
 	local val="$*"
 	local bytes
 	let bytes=$(echo -n "$val"|wc -c)
-	mc_sendmsg "$command $key 0 $exptime $bytes\r\n$val"
+	mc-sendmsg "$command $key 0 $exptime $bytes\r\n$val"
 }
 
-mc_set() { mc_doset set "$@";}
-mc_add() { mc_doset add "$@";}
-mc_replace() { mc_doset replace "$@";}
-mc_append() { mc_doset append "$@";}
-mc_prepend() { mc_doset prepend "$@";}
+mc-set() { mc-doset set "$@";}
+mc-add() { mc-doset add "$@";}
+mc-replace() { mc-doset replace "$@";}
+mc-append() { mc-doset append "$@";}
+mc-prepend() { mc-doset prepend "$@";}
 
-mc_delete() { mc_sendmsg delete "$*";}
-mc_incr() { mc_sendmsg incr "$*";}
-mc_decr() { mc_sendmsg decr "$*";}
+mc-delete() { mc-sendmsg delete "$*";}
+mc-incr() { mc-sendmsg incr "$*";}
+mc-decr() { mc-sendmsg decr "$*";}
 
-mc_superpurge() {
-	mc_list_all_keys > /dev/null
-	if [ ! -z "/dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt" ];then
-		grep "$1" /dev/shm/mc_all_keys_${MCSERVER}_${MCPORT}.txt >/dev/shm/temp.swap.${MCSERVER}_${MCPORT}.txt
+mc-superpurge() {
+	mc-list-all-keys > /dev/null
+	if [ ! -z "/dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt" ];then
+		grep "$1" /dev/shm/mc-all-keys-${MCSERVER}-${MCPORT}.txt >/dev/shm/temp.swap.${MCSERVER}-${MCPORT}.txt
 	fi
 	while read keys; do
-		mc_sendmsg "delete ${keys}"
-	done </dev/shm/temp.swap.${MCSERVER}_${MCPORT}.txt
+		mc-sendmsg "delete ${keys}"
+	done </dev/shm/temp.swap.${MCSERVER}-${MCPORT}.txt
 
-	rm -rf /dev/shm/temp.swap.${MCSERVER}_${MCPORT}.txt
+	rm -rf /dev/shm/temp.swap.${MCSERVER}-${MCPORT}.txt
 }
 
 if [ "$(basename "$0" .sh)" = "lib-memcache" ]; then
@@ -138,7 +138,7 @@ if [ "$(basename "$0" .sh)" = "lib-memcache" ]; then
 	[ -z "$command" ] && command="usage"
 	let OPTIND++
 
-	mc_$command "${@:$OPTIND}"
+	mc-$command "${@:$OPTIND}"
 
 	exit $?
 fi
