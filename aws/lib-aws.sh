13a09db881d4b87866583d031dbb951864c22da3
Joan Fisbein <joan@fon.com>
2016-03-14 10:52:48 +0000

    Renamed functions to comply with new naming

--- aws/lib-aws.sh
+++ aws/lib-aws.sh
@@ -8,7 +8,7 @@ source "$DIR/bash-lib/aws/lib-aws-s3.sh"
 source "$DIR/bash-lib/aws/lib-aws-apigateway.sh"
 
 # Return Cognito pool id by name
-function getCognitoPoolIdByName() {
+function cognito-get-pool-id-by-name() {
 	local POOL_NAME=$1
 	local POOL_ID=$(aws cognito-identity list-identity-pools --max-results 60 --query "IdentityPools[?IdentityPoolName=='$POOL_NAME'].IdentityPoolId" --output text)
 
@@ -17,7 +17,7 @@ function getCognitoPoolIdByName() {
 
 # Verify if a Cognito pool exists
 # Return 0 if the pool exists, 1 otherwise
-function cognitoPoolExist() {
+function cognito-check-if-pool-exist() {
 	local POOL_NAME=$1
 	if [[ $(aws cognito-identity list-identity-pools --max-results 60 --query "IdentityPools[?IdentityPoolName=='$POOL_NAME'] | length(@)" --output text) == "1" ]]; then
 		return 0
@@ -28,7 +28,7 @@ function cognitoPoolExist() {
 
 # Verify if a Lambda function exists
 # Return 0 if the function exists, 1 otherwise
-function lambdaFunctionExists() {
+function lambda-check-if-function-exists() {
 	local LAMBDA_FUNCTION_NAME=$1
 	if [[ $(aws lambda list-functions --query "Functions[?FunctionName=='$LAMBDA_FUNCTION_NAME'] | length (@)") == "1" ]]; then
 		return 0
@@ -37,14 +37,14 @@ function lambdaFunctionExists() {
 	fi
 }
 
-function getLambdaArnByName() {
+function lambda-get-arn-by-name() {
 	local LAMBDA_FUNCTION_NAME=$1
 	local ARN=$(aws lambda get-function --function-name "$LAMBDA_FUNCTION_NAME" --query "Configuration.FunctionArn" --output text)
 
 	echo "$ARN"
 }
 
-function getListSize() {
+function get-list-size() {
 	LIST=$1
 	COUNTER=0
 	for ELEM in $LIST; do
