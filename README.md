dc0ab0c9639bbb110333e02f5067ab01aa331109
Joan Fisbein <joan@fon.com>
2016-04-04 14:43:01 +0200

    Added BashDoc to Jenkins library

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ This project host a bunch of bash libraries created by myself.
 + lib-gitlab.sh: library to use gitlab api.
 + lib-log.sh: ANSI color logging
 + lib-memcache.sh: simple client for memcache
++ lib-jenkins.sh: jenkins-cli.jar wrapper to interact with Jenkins API
 + aws/lib-aws.sh includes all the below files:
   + aws/lib-aws-apigateway.sh aws api gateway helper methods
   + aws/lib-aws-dynamodb.sh dynamo db helper methods
   + aws/lib-aws-ec2.sh ec2 helper methods
   + aws/lib-aws-iam.sh iam helper methods
-  + aws/lib-aws-ec2.sh s3 helper methods
+  + aws/lib-aws-s3.sh s3 helper methods
 
-The libraries are very incomplete, they are in continous evolution and I add methods when I need them
-  
+The libraries are very incomplete, they are in continous evolution and I add methods when I need them.
--- lib-jenkins.sh
+++ lib-jenkins.sh
@@ -1,34 +1,43 @@
-JENKINS_HOST="http://jenkins.fon.ofi:8080/"
+JENKINS_API_URL="http://jenkins.fon.ofi:8080/"
 JENKINS=""
 
+# Init Jenkins library, use it to set Jenkins API URL and downloads jenkins-cli.jar
+# Param 1: [Optional] Jenkins API URL (defaults to http://jenkins.fon.ofi:8080/)
 function jenkins-init() {
 	if [ $# -eq 0 ]; then
-		JENKINS="java -jar jenkins-cli.jar -s $JENKINS_HOST/"
-		_jenkins-download-cli $JENKINS_HOST
+		JENKINS="java -jar jenkins-cli.jar -s $JENKINS_API_URL/"
+		_jenkins-download-cli $JENKINS_API_URL
 		return 0
 	elif [ $# -eq 1 ]; then
-		JENKINS_HOST="$1"
-		JENKINS="java -jar jenkins-cli.jar -s $JENKINS_HOST/"
-		_jenkins-download-cli $JENKINS_HOST
+		JENKINS_API_URL="$1"
+		JENKINS="java -jar jenkins-cli.jar -s $JENKINS_API_URL/"
+		_jenkins-download-cli $JENKINS_API_URL
 		return 0
 	else
 		return 1
 	fi
 }
 
+# Downloads if not present jenkins-cli.jar
+# Param 1: Jenkins API URL
 function _jenkins-download-cli() {
-	local JENKINS_HOST=$1
+	local JENKINS_API_URL=$1
 
 	if [ ! -f jenkins-cli.jar ]; then
-		wget -q $JENKINS_HOST/jnlpJars/jenkins-cli.jar
+		wget -q $JENKINS_API_URL/jnlpJars/jenkins-cli.jar
 	fi
 }
 
+# Dumps the job definition XML to stdout.
+# Param 1: Job name
 function jenkins-get-job() {
 	local PROJECT=$1
 	$JENKINS get-job $PROJECT
 }
 
+# Updates the job definition XML from a file
+# Param 1: Job name
+# Param 2: Job definition file
 function jenkins-update-job() {
 	local PROJECT=$1
 	local JOB_FILE="$2"
@@ -36,24 +45,32 @@ function jenkins-update-job() {
 	cat "$JOB_FILE" | $JENKINS update-job $PROJECT
 }
 
-function jenkins-update-job() {
+# Reload job
+# Param 1: Job name
+function jenkins-reload-job() {
 	local PROJECT=$1
 	$JENKINS reload-job $PROJECT
 }
 
+# Enables a job.
+# Param 1: Job name
 function jenkins-enable-job() {
 	local PROJECT=$1
 	$JENKINS enable-job $PROJECT
 }
 
+# Copies a job.
+# Param 1: Source job
+# Param 1: Destination job
 function jenkins-copy-job() {
 	local SOURCE=$1
 	local DEST=$2
 	$JENKINS copy-job $SOURCE $DEST
 }
 
-
-
+# Modifies a file containing a job definition XML to set the Git URL
+# Param 1: Git URL
+# Param 2: Job definition file
 function jenkins-job-set-git-url() {
 	local GIT_URL="$1"
 	local JOB_FILE="$2"
